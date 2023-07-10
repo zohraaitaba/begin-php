c6a6b18ee43371699ad897f69852c17165072230
Zohra AIT ABA <youbi.zohra@gmail.com>
2023-07-10 15:03:57 +0200

    first class

--- 11-poo/index.php
+++ 11-poo/index.php
@@ -4,7 +4,20 @@ use App\Cat;
 
 require 'vendor/autoload.php';
 
+$bianca = new Cat('Bianca');
+$bianca->setFur('blanc')->setFur('noir');
+$mina = new Cat('Mina');
+$mina->setFur('noir');
 
+dump($bianca, $mina);
 
-$bianca = new Cat();
-var_dump($bianca);
+?>
+
+<h1>Mon premier chat s'appelle <?= $bianca->getName(); ?></h1>
+<p><?= $bianca->cry(); ?></p>
+<p>
+    l'autre chat  est <?= $mina->getName(); ?>
+    Et <?= $mina->cry(); ?>
+</p>
+
+<p><?= $bianca->playWith($mina); ?></p>
--- 11-poo/src/Cat.php
+++ 11-poo/src/Cat.php
@@ -1,8 +1,50 @@
-<?php 
+<?php
 
 namespace App;
 
 class Cat
 {
+    private string $name;
+    private string $type = 'Chat de gouttière';
+    private string $fur;
 
+    public function __construct(string $name, string $type = 'Chat de gouttière')
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+
+
+    /**
+     * Set the value of fur
+     *
+     * @return  self
+     */
+    public function setFur(string $fur): self
+    {
+        $this->fur = $fur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+//fonction chat miaule
+    public function cry(): string
+    {
+        return $this->name.' miaule';
+    }
+
+ // fonction chat joue avec
+    public function playWith(Cat $otherCat): string
+    {
+        return $this->name. ' joue avec '.$otherCat->name;
+    }
 }
